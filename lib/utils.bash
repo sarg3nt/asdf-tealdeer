4e9229154f3824755c2361ad966679b41d8cff0d
Dave Sargent <dave_sargent@selinc.com>
2024-05-23 14:11:04 -0700

    Fix test

--- lib/utils.bash
+++ lib/utils.bash
@@ -64,8 +64,7 @@ install_version() {
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		ls -alh "$install_path/${tool_cmd}"
-		"$install_path/${tool_cmd}" --version
-		test -x "$install_path/${tool_cmd} --version" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/${tool_cmd}" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
